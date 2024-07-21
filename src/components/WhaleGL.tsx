15b2d6ca44ac41ee0e6d5174dc4a6e27e231e386
kohama66 <54892265+kohama66@users.noreply.github.com>
2024-07-21 19:42:55 +0900

    feat: whale

--- src/components/WhaleGL.tsx
+++ src/components/WhaleGL.tsx
@@ -1,22 +1,106 @@
-import { Grid, OrbitControls, useGLTF } from "@react-three/drei";
+import {
+  Environment,
+  Float,
+  Lightformer,
+  MeshTransmissionMaterial,
+  OrbitControls,
+  RoundedBox,
+  useGLTF,
+} from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import type React from "react";
+import { Color } from "three";
 
 export const WhaleGL: React.FC = () => {
+  const speed = 10;
   return (
-    <Canvas
-      style={{ width: "100vw", height: "100vh", backgroundColor: "#151321" }}
-      camera={{ position: [0.5, -0.2, -0.5], fov: 60 }}
-    >
-      <hemisphereLight intensity={2} color="#8e82e5" groundColor="#aa78cc" />
-      <Whale />
-      <OrbitControls makeDefault position={[5, 2, 5]} target={[0, 0, -0.1]} />
-    </Canvas>
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <Canvas
+        camera={{ position: [0, 0, -2.5] }}
+        style={{ backgroundColor: "#ededed" }}
+      >
+        <OrbitControls autoRotate autoRotateSpeed={4} />
+
+        <ambientLight intensity={1.2} />
+        <Environment resolution={1024}>
+          <group rotation={[-Math.PI / 2, 0, 0]}>
+            <Lightformer
+              intensity={4}
+              rotation-x={Math.PI / 2}
+              position={[0, 5, -9]}
+              scale={[10, 10, 1]}
+            />
+            {[2, 0, 2, 0, 2, 0, 2, 0].map((x, i) => (
+              <Lightformer
+                key={i}
+                form="circle"
+                intensity={4}
+                rotation={[Math.PI / 2, 0, 0]}
+                position={[x, 4, i * 4]}
+                scale={[4, 1, 1]}
+              />
+            ))}
+            <Lightformer
+              intensity={2}
+              rotation-y={Math.PI / 2}
+              position={[-5, 1, -1]}
+              scale={[50, 2, 1]}
+            />
+            <Lightformer
+              intensity={12}
+              rotation-y={-Math.PI / 8}
+              position={[10, 1, 0]}
+              scale={[50, 2, 1]}
+            />
+          </group>
+        </Environment>
+
+        <Aquarium>
+          <Float rotationIntensity={1} floatIntensity={1} speed={speed / 2}>
+            <Whale />
+          </Float>
+        </Aquarium>
+      </Canvas>
+    </div>
   );
 };
 
 const Whale: React.FC = () => {
   const { scene } = useGLTF("whale.glb");
 
-  return <primitive object={scene} />;
+  return <primitive object={scene} scale={[2, 2, 2]} />;
+};
+
+const Aquarium: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  return (
+    <group rotation={[0, 1.5, 0]}>
+      <mesh position={[0, 0, 0]}>
+        {/* <sphereGeometry args={[1, 64, 64]} /> */}
+        {/* <boxGeometry args={[1, 1.2, 2]} /> */}
+        <RoundedBox
+          args={[1, 1.2, 2]} // サイズ
+          radius={0.05} // 角の半径
+          smoothness={4} // 丸角の滑らかさ
+          position={[0, 0, 0]} // 位置
+        >
+          <MeshTransmissionMaterial
+            ior={1.33} // 水の屈折率に近い値
+            background={new Color("#c6e5db")}
+            backside
+            samples={4}
+            thickness={0.5}
+            chromaticAberration={0.025}
+            anisotropy={0.1}
+            distortion={0.2}
+            distortionScale={4}
+            temporalDistortion={1}
+            iridescence={1}
+            iridescenceIOR={1}
+            iridescenceThicknessRange={[0, 1400]}
+          />
+        </RoundedBox>
+      </mesh>
+      <group>{children}</group>
+    </group>
+  );
 };
